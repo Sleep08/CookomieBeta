1c1916de82395e6fc13045023f864267d0ccd158
Sleep08 <88199254+Sleep08@users.noreply.github.com>
2022-04-14 01:52:46 +0200

    Add files via upload

--- Scripts/buttonportail.js
+++ Scripts/buttonportail.js
@@ -1,89 +1,65 @@
 const buttonPortail = document.querySelector(".buttonDiscover")
 const leftPart = document.querySelector(".leftPart");
 const portailCookomie = document.querySelector(".portail");
-const plusPhilo = document.getElementById("plusPhilosophie");
-const titlePhilo = document.getElementById("titlePhilo");
-let philoPart = document.querySelector(".leftPart__philosophie");
-let menuPart = document.querySelector(".leftPart__balade");
-let philoComment = document.querySelector(".philoComment");
-let circlePhilo = document.getElementById("plusCirclePhilo");
-const plusChef = document.getElementById("plusChef");
-const titleChef = document.getElementById("titleChef");
-let chefPart = document.querySelector(".rightPart");
-let chefComment = document.querySelector(".chefComment");
-let circleChef = document.getElementById("plusCircleChef");
-const plusMenu = document.getElementById("plusMenu");
-const titleMenu = document.getElementById("titleMenu");
-let menuComment = document.querySelector(".menuComment");
-let circleMenu = document.getElementById("plusCircleMenu");
-const titleMenuDeux = document.getElementById("titleMenuDeux");
+const galerieButton = document.querySelector(".galleryMenu");
+const mainSection = document.getElementById("accueil");
+const galerieSection = document.getElementById("galerieCookomie");
+const returnAccueil = document.querySelector(".returnAccueil");
+const menuNavigLogo = document.querySelector(".fa-bars");
+const menuNavig = document.querySelector("#cookomieMenu");
+const buttonMenu = document.querySelector(".buttonMenu");
+const philoPart = document.querySelector("#philosophieCookomie");
+const philoButton = document.querySelector(".sectionOne");
+const philoReturn = document.querySelector(".returnAccueilPhilo")
+const cheffeButton = document.querySelector(".sectionTwo");
+const cheffePart = document.getElementById("votreCheffe");
+const cheffeReturn = document.querySelector(".returnAccueilCheffe");
 
 buttonPortail.addEventListener("click", function(){
     portailCookomie.style.display = "none";
 });
 
-circlePhilo.addEventListener("click", function() {
-    titlePhilo.style.width = "95%";
-    titlePhilo.style.fontSize = "2em";
-    philoComment.style.display = "block";
-    circlePhilo.style.display = "none";
-    let moinsPhilo = document.createElement("i");
-    moinsPhilo.setAttribute("class", "fa-solid fa-circle-minus");
-    moinsPhilo.setAttribute("id", "moinsPhilo");
-    plusPhilo.appendChild(moinsPhilo);
-    moinsPhilo.addEventListener("click", function() {
-        titlePhilo.style.width = "80%";
-        titlePhilo.style.fontSize = "3em";
-        philoComment.style.display = "none";
-        circlePhilo.style.display = "";
-        moinsPhilo.style.display = "none";
-    })
-});
+galerieButton.addEventListener("click", function(){
+    mainSection.style.display = "none";
+    galerieSection.style.display = "block";
+})
 
-circleChef.addEventListener("click", function() {
-    titleChef.style.width = "95%";
-    titleChef.style.fontSize = "2em";
-    chefComment.style.display = "block";
-    circleChef.style.display = "none";
-    let moinsChef = document.createElement("i");
-    moinsChef.setAttribute("class", "fa-solid fa-circle-minus");
-    moinsChef.setAttribute("id", "moinsChef");
-    plusChef.appendChild(moinsChef);
-    document.querySelector(".leftPart__philosophie").style.display="none";
-    document.querySelector(".leftPart__balade").style.display="none";
-    let imgLeft = document.createElement("img");
-    imgLeft.setAttribute("src", "cookomiepictures/joPresa.jpg");
-    leftPart.appendChild(imgLeft);
-    moinsChef.addEventListener("click", function() {
-        titleChef.style.width = "80%";
-        titleChef.style.fontSize = "3em";
-        chefComment.style.display = "none";
-        circleChef.style.display = "";
-        moinsChef.style.display = "none";
-        leftPart.removeChild(imgLeft);
-        document.querySelector(".leftPart__philosophie").style.display="";
-        document.querySelector(".leftPart__balade").style.display="";
-    })
-});
+returnAccueil.addEventListener("click", function() {
+    mainSection.style.display = "";
+    galerieSection.style.display = "none";
+})
 
-circleMenu.addEventListener("click", function() {
-    menuComment.style.display = "block";
-    circleMenu.style.display = "none";
-    let moinsMenu = document.createElement("i");
-    moinsMenu.setAttribute("class", "fa-solid fa-circle-minus");
-    moinsMenu.setAttribute("id", "moinsMenu");
-    plusMenu.appendChild(moinsMenu);
-    titleChef.style.display="none";
-    chefPart.style.backgroundImage = "url(cookomiepictures/roseH.jpg)";
-    philoPart.style.display="none";
-    menuPart.style.height="100%";
-    moinsMenu.addEventListener("click", function() {
-        titleChef.style.display = "";
-        menuComment.style.display = "none";
-        circleMenu.style.display = "";
-        moinsMenu.style.display = "none";
-        chefPart.style.backgroundImage = "url(cookomiepictures/jo.jpg)";
-        philoPart.style.display="";
-        menuPart.style.height="50%";
-    });
-});
+menuNavigLogo.addEventListener("click", function(){
+    menuNavig.style.display = "block";
+    menuNavigLogo.style.display = "none"
+    let crossClose = document.createElement("i");
+    crossClose.setAttribute("class", "fa-solid fa-xmark");
+    buttonMenu.appendChild(crossClose);
+    crossClose.addEventListener("click", function() {
+        menuNavig.style.display="none";
+        crossClose.style.display = "none";
+        menuNavigLogo.style.display = "";
+    })
+})
+
+philoButton.addEventListener("click", function() {
+    mainSection.style.display = "none";
+    philoPart.style.display = "block";
+})
+
+philoReturn.addEventListener("click", function() {
+    mainSection.style.display = "";
+    philoPart.style.display = "none";
+})
+
+cheffeButton.addEventListener("click", function() {
+    mainSection.style.display = "none";
+    philoPart.style.display = "none";
+    cheffePart.style.display = "block";
+})
+
+cheffeReturn.addEventListener("click", function() {
+    mainSection.style.display = "";
+    cheffePart.style.display = "none";
+})
+
